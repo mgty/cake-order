52d60fc18369516d022f1ab97b48f49d85faad3b
mgty <marygracety@gmail.com>
2020-02-12 16:19:29 -0500

    updated php file and styled result page

--- cake.php
+++ cake.php
@@ -5,57 +5,44 @@
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
-    <h2>Sweet Lily's Bakery</h2>
-    <h4>Your Cake Order</h4>
+
     <?php        
 
         $size_price = ["Small" => 80, "Medium" => 150, "Large" => 250];
 
         //size
         $size = filter_input(INPUT_POST, 'size');
+        $size_message="Size ($$size_price[$size]): $size";
         if($size == NULL){
-                echo "Please choose a cake size.";
+                echo "Please select a cake size.";
             } else {
-                echo "Size: $size @ $$size_price[$size]<br>";
+                echo "";
             };
 
 
         //frosting
         $frosting = filter_input(INPUT_POST, 'frosting');
         $frosting_price = ["chocolate", "vanilla", "strawberry", "caramel"];
+        $frosting_message="Frosting ($20): $frosting<br>";
 
             if($frosting == NULL){
                 echo "Please choose a frosting.";
             } else {
-                echo "Selected Frosting: $frosting. Great Choice!<br>";
+                echo "";
             };
 
 
         //toppings
         $toppings = filter_input(INPUT_POST, 'toppings', 
             FILTER_SANITIZE_SPECIAL_CHARS, FILTER_REQUIRE_ARRAY);
-        
-        
-        if ($toppings != NULL ){
-           echo "Topping(s) ($20 each): <br>";
-                foreach ($toppings as $key => $value) {
-                echo "&#8226; $value<br>";
-            }
-        } else {
-            echo "No toppings selected..<br>";
-        }
+
+    
 
         //extras
 
         $extras = filter_input(INPUT_POST, 'extras',
             FILTER_SANITIZE_SPECIAL_CHARS, FILTER_REQUIRE_ARRAY);
 
-        if ($extras != NULL){
-            echo "Extra(s) ($10 each): <br>";
-            foreach ($extras as $key3 => $value3) {
-                echo "&#8226; $value3<br>";
-            }
-        }
 
         /* if ($toppings > 1){
             $num_toppings = count($toppings);
@@ -93,10 +80,7 @@
         //requests
         $requests = filter_input(INPUT_POST, 'requests');
         $requests = nl2br($requests, false);
-        if ($requests != ''){
-            echo "You made this request: <br> '<i>$requests</i>'.<br>
-             We will do our best to honor it."; 
-        }
+        
 
 
         //Final Calculation:
@@ -106,20 +90,64 @@
         $total_extras = count($extras) * 10;
         $total = $total_size_price + $total_frosting_price + $total_toppings + $total_extras;
         $phone_number = filter_input(INPUT_POST, 'phone_number');
-        $address1 = filter_input(INPUT_POST, 'address1');
-        $address2 = filter_input(INPUT_POST, 'address2');
+        $address = filter_input(INPUT_POST, 'address');
+        $full_name = filter_input(INPUT_POST, 'full_name');
 
         $total_price_formatted = "$".number_format($total, 2); 
 
     ?>
 <br>
-<p>You total amount due on delivery is <?php echo $total_price_formatted;?></p>
-<p>Your cake will be delivered to: 
-<?php echo "$address1 <br> $address2"?>
-<br>
-We will call phone number: <?php echo " $phone_number when it's on the way. "?>
-Thanks for ordering at Sweet Lily's Bakery!
-</p>
+<div class="receipt-page-container container">
+    <div class="row">
+            <div class="second-column col-sm-12">
+                <p class="page-title">Sweet Lily's Bakery - Order Form</p>
+                <p class="option-title"><?php echo"$size_message"?></p>
+                <p class="option-title"><?php echo"$frosting_message"?></p>
+                <p class="option-title">
+                    <?php 
+                        if ($toppings != NULL ){
+                           echo "Topping(s) ($20 each): <br>";
+                                foreach ($toppings as $key => $value) {
+                                echo "&#8226; $value<br>";
+                            }
+                        } else {
+                            echo "No toppings selected..<br>";
+                        }
+                    ?>
+                </p>
+
+                <p class="option-title">
+                    <?php
+                        if ($extras != NULL){
+                            echo "Extra(s) ($10 each): <br>";
+                            foreach ($extras as $key3 => $value3) {
+                                echo "&#8226; $value3<br>";
+                            }
+                        }
+                    ?>
+                </p>
+                <p class="option-title">
+                    <?php
+                        if ($requests != ''){
+                            echo "You made this request: <br> '<i>$requests</i>'.<br>
+                             We will do our best to honor it."; 
+                        }
+                    ?>
+                </p>
+
+                <p class="option-title">Your total amount due on delivery is <?php echo $total_price_formatted;?>
+                <br>
+                Thanks for ordering <?php echo $full_name;?>. Your cake will be delivered to: 
+                <?php echo "$address."?>
+                <br>
+                We will call phone number: <?php echo " $phone_number when it's on the way. <br><br>"?>
+
+                Thanks for ordering at Sweet Lily's Bakery!
+                </p>
+            </div>
+
+        </div>
+</div>
 
 </body>
 </html>
